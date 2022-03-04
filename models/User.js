2f20eff4a7587c1448945cf0f3dd0327a9c0ff8e
Ruchit-P <56946935+ruchit-p@users.noreply.github.com>
2022-03-04 00:43:12 -0500

    defined MongoDB URI and Schema

--- models/User.js
+++ models/User.js
@@ -1,5 +1,26 @@
 const mongoose = require('mongoose')
 
 const bcrypt = require('bcrypt')
+require('dotenv').config()
 
-mongoose.connect()
+mongoose.connect(MONGODB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    email:{
+        type: String,
+        unique:true,
+        required:true
+    },
+    password:{
+        type:String,
+        required: true
+    }
+})
